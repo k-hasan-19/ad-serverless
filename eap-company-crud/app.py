bba296362eaf57119a99251a11add0d2c7938c2e
EC2 Default User <ec2-user@ip-172-31-7-32.us-west-2.compute.internal>
2020-01-17 13:27:24 +0000

    first commit

---
+++ eap-company-crud/app.py
@@ -0,0 +1,114 @@
+import os
+import boto3
+import json
+import decimal
+
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+from encoder_class import DecimalEncoder
+
+
+
+def get_company_meta(event, context):
+    
+    table = __get_table_client()
+    company_id = event["pathParameters"]["company_id"]
+    PK, SK = _get_company_meta_keys(company_id)
+    print("Key: ", json.dumps({'PK':PK, 'SK':SK}, indent=4))
+
+    try:
+        data = table.get_item(Key={"PK":PK, "SK":SK}, ReturnConsumedCapacity='TOTAL')
+        
+        company_meta_info = _parse_company_details(data["Item"])
+        
+        # response = table.get_item(Key={"PK":"COMPANY#8fd4728b-89b6-40aa-a57a-85a4672ec9a0", "SK":"#METADATA#8fd4728b-89b6-40aa-a57a-85a4672ec9a0"}, ReturnConsumedCapacity='TOTAL')
+
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return _response(500, {'status':"DynamoDB Client Error"})
+    except KeyError as e:
+        print(e)
+        return _response(404, {'status':"ITEM NOT FOUND"})
+    else:
+        consumed_cap = data["ConsumedCapacity"]
+        print("GetItem succeeded:")
+        print(json.dumps(data, indent=4, cls=DecimalEncoder))
+        print(json.dumps(consumed_cap, indent=4, cls=DecimalEncoder))
+        
+        
+    
+    return _response(200, company_meta_info)
+    
+
+def put_company_meta(event, context):
+    
+    import uuid
+    table = __get_table_client()
+    company_id = str(uuid.uuid4())
+    PK, SK = _get_company_meta_keys(company_id)
+    payload = event['body']
+    payload['PK'] = PK
+    payload['SK'] = SK
+    
+    print("Key: ", json.dumps({'PK':PK, 'SK':SK}, indent=4))
+
+    try:
+        data = table.put_item(Item=payload, ReturnValues='UPDATED_NEW', ReturnConsumedCapacity='TOTAL' )
+        
+        company_meta_info = _parse_company_details(data["Item"])
+        
+        # response = table.get_item(Key={"PK":"COMPANY#8fd4728b-89b6-40aa-a57a-85a4672ec9a0", "SK":"#METADATA#8fd4728b-89b6-40aa-a57a-85a4672ec9a0"}, ReturnConsumedCapacity='TOTAL')
+
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return _response(500, {'status':"DynamoDB Client Error"})
+    except KeyError as e:
+        print(e)
+        return _response(404, {'status':"ITEM NOT FOUND"})
+    else:
+        consumed_cap = data["ConsumedCapacity"]
+        print("GetItem succeeded:")
+        print(json.dumps(data, indent=4, cls=DecimalEncoder))
+        print(json.dumps(consumed_cap, indent=4, cls=DecimalEncoder))
+        
+    return _response(201, company_meta_info)
+    
+    
+''' Company info cleaner'''
+
+def _parse_company_details(item):
+    item.pop('PK', None)
+    item.pop('SK', None)
+    return item
+
+''' Company partition key generator '''
+
+def _get_company_meta_keys(company_id):
+    PK = "COMPANY#" + company_id
+    SK = "#METADATA#" + company_id
+    return (
+        PK,
+        SK,
+    )
+
+# Http response builder
+
+def _response(status_code, json_body):
+    body = json.dumps(json_body)
+
+    return {
+        "statusCode": status_code,
+        "headers": {
+            "Content-Type": "application/json",
+            # "Access-Control-Allow-Origin": "*"
+        },
+        "body": body,
+    }
+
+
+def __get_table_client():
+    TABLE_NAME = os.getenv("TABLE_NAME")
+    AWS_REGION_DYNAMODB = os.getenv('AWS_REGION_DYNAMODB')
+    dynamodb = boto3.resource("dynamodb", region_name=AWS_REGION_DYNAMODB)
+    table = dynamodb.Table(TABLE_NAME)
+    return table
