e8c0b8351566f82c4ae0e27e35b3a97873d764d1
Kamrul Hasan <kamrul@inneed.cloud>
2020-02-07 10:44:15 +0000

    update user crud API

--- user-crud/app.py
+++ user-crud/app.py
@@ -11,126 +11,127 @@ from encoder_class import DecimalEncoder
 
 
 def crud_handler(event, context):
-       
-    if event['httpMethod']=='GET':
+
+    if event["httpMethod"] == "GET":
         response = get_user_meta(event, context)
         return response
-    if event['httpMethod']=='POST':
+    if event["httpMethod"] == "POST":
         response = post_user_meta(event, context)
         return response
-    if event['httpMethod']=='PUT':
+    if event["httpMethod"] == "PUT":
         response = put_user_meta(event, context)
         return response
 
 
-
-
 def get_user_meta(event, context):
-    
+
     table = __get_table_client()
-    company_id = event["queryStringParameters"]["company_id"]
-    user_id = event["queryStringParameters"]["user_id"]
-    
+    company_id = event["pathParameters"]["company_id"]
+    user_id = event["pathParameters"]["user_id"]
+
     PK, SK = UserMeta.keys_from_ids(company_id, user_id)
-    print("Key: ", json.dumps({'PK':PK, 'SK':SK}, indent=4))
+    print("Key: ", json.dumps({"PK": PK, "SK": SK}, indent=4))
 
     try:
-        data = table.get_item(Key={"PK":PK, "SK":SK}, ReturnConsumedCapacity='TOTAL')
-        
+        data = table.get_item(Key={"PK": PK, "SK": SK}, ReturnConsumedCapacity="TOTAL")
+
         user = UserMeta(data["Item"])
 
     except ClientError as e:
-        print(e.response['Error']['Message'])
-        return _response(500, {'status':"DynamoDB Client Error"})
+        print(e.response["Error"]["Message"])
+        return _response(500, {"status": "DynamoDB Client Error"})
     except KeyError as e:
         print(e)
-        return _response(404, {'status':"ITEM NOT FOUND"})
+        return _response(404, {"status": "ITEM NOT FOUND"})
     else:
         consumed_cap = data["ConsumedCapacity"]
         print("GetItem succeeded:")
         print(json.dumps(data, indent=4, cls=DecimalEncoder))
-        
-        
-    
+
     return _response(200, user.get_item())
-    
+
+
 def post_user_meta(event, context):
-    
+
     table = __get_table_client()
-    
-    payload = json.loads(event['body'])
+    company_id = event["pathParameters"]["company_id"]
+    payload = json.loads(event["body"])
+    payload.update({"company_id": company_id})
     user = UserMeta(payload)
     keys = user.get_keys()
-    PK, SK = keys['PK'], keys['SK']
-    print("Key: ", json.dumps({'PK':PK, 'SK':SK}, indent=4))
+    PK, SK = keys["PK"], keys["SK"]
+    print("Key: ", json.dumps({"PK": PK, "SK": SK}, indent=4))
     table_record = user.get_record()
-    
 
     try:
-        table.put_item(Item=table_record, ConditionExpression='attribute_not_exists(PK) and attribute_not_exists(SK)' ,ReturnConsumedCapacity='TOTAL')
+        table.put_item(
+            Item=table_record,
+            ConditionExpression="attribute_not_exists(PK) and attribute_not_exists(SK)",
+            ReturnConsumedCapacity="TOTAL",
+        )
     except ClientError as e:
-        if e.response['Error']['Code']=='ConditionalCheckFailedException':
-            return _response(409, {'status':"Item already exists"})
-        print(e.response['Error']['Message'])
-        return _response(500, {'status':"DynamoDB Client Error"})
+        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+            return _response(409, {"status": "Item already exists"})
+        print(e.response["Error"]["Message"])
+        return _response(500, {"status": "DynamoDB Client Error"})
     else:
         print("PutItem succeeded:")
         print(json.dumps(table_record, indent=4, cls=DecimalEncoder))
     item = user.get_item()
     return _response(201, item)
-    
+
 
 def put_user_meta(event, context):
-    
+
     table = __get_table_client()
-    
-    payload = json.loads(event['body'])
-    
+    company_id = event["pathParameters"]["company_id"]
+    user_id = event["pathParameters"]["user_id"]
+    payload = json.loads(event["body"])
+    payload.update({"company_id": company_id, "user_id": user_id})
     user = UserMeta(payload)
     table_record = user.get_record()
-    PK = table_record['PK']
-    SK = table_record['SK']
+    PK = table_record["PK"]
+    SK = table_record["SK"]
     try:
-        response = table.update_item(
-            Key={
-                'PK': PK,
-                'SK': SK
-            },
-            ConditionExpression='attribute_exists(PK) and attribute_exists(SK)',
-            UpdateExpression='SET #company_id = :company_id, #first_name = :first_name, #last_name = :last_name, #address = :address, #updated_at = :updated_at',
-            ExpressionAttributeNames={
-                '#company_id': 'company_id',
-                '#first_name': 'first_name',
-                '#last_name': 'last_name',
-                '#address': 'address',
-                '#updated_at':'updated_at'
-            },
-            ExpressionAttributeValues={
-                ':first_name': table_record['first_name'],
-                ':last_name': table_record['last_name'],
-                ':address': table_record['address'],
-                ':updated_at': table_record['updated_at']
-            },
-            ReturnValues='ALL_NEW',
-            ReturnConsumedCapacity='TOTAL'
+        response = table.put_item(
+            ConditionExpression="attribute_exists(PK) and attribute_exists(SK)",
+            # UpdateExpression="SET #company_id = :company_id, #first_name = :first_name, #last_name = :last_name, #address = :address, #updated_at = :updated_at",
+            # ExpressionAttributeNames={
+            #     "#company_id": "company_id",
+            #     "#first_name": "first_name",
+            #     "#last_name": "last_name",
+            #     "#address": "address",
+            #     "#updated_at": "updated_at",
+            # },
+            # ExpressionAttributeValues={
+            #     ":first_name": table_record["first_name"],
+            #     ":last_name": table_record["last_name"],
+            #     ":address": table_record["address"],
+            #     ":updated_at": table_record["updated_at"],
+            # },
+            Item=table_record,
+            ReturnConsumedCapacity="TOTAL",
         )
 
     except ClientError as e:
-        if e.response['Error']['Code']=='ConditionalCheckFailedException':
-            return _response(404, {'status':"ITEM NOT FOUND"})
-            
-        print(e.response['Error']['Message'])
-        return _response(500, {'status':"DynamoDB Client Error"})
+        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+            return _response(404, {"status": "ITEM NOT FOUND"})
+
+        print(e.response["Error"]["Message"])
+        return _response(500, {"status": "DynamoDB Client Error"})
     else:
         print("PutItem succeeded:")
         print(json.dumps(table_record, indent=4, cls=DecimalEncoder))
-    print('Item update response: ',response)
-    if not response.get('Attributes'):
-        return _response(404, {'status':"ITEM NOT FOUND"})
-    item = UserMeta(response['Attributes']).get_item()
+    # print("Item update response: ", response)
+    # if not response.get("Attributes"):
+    #     return _response(404, {"status": "ITEM NOT FOUND"})
+    # item = UserMeta(response["Attributes"]).get_item()
+    item = user.get_item()
     return _response(200, item)
-    
+
+
 # Http response builder
+
 
 def _response(status_code, json_body):
     body = json.dumps(json_body)
@@ -147,7 +148,7 @@ def _response(status_code, json_body):
 
 def __get_table_client():
     TABLE_NAME = os.getenv("TABLE_NAME")
-    AWS_REGION_DYNAMODB = os.getenv('AWS_REGION_DYNAMODB')
+    AWS_REGION_DYNAMODB = os.getenv("AWS_REGION_DYNAMODB")
     dynamodb = boto3.resource("dynamodb", region_name=AWS_REGION_DYNAMODB)
     table = dynamodb.Table(TABLE_NAME)
     return table
